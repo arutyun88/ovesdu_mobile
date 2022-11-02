da6db94a23e6df5688741f9288f23e3c747e4a6e
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-03 02:47:31 +0400

    app: ui: style: font: nunito assets

--- lib/app/ui/config/app_fonts.dart
+++ lib/app/ui/config/app_fonts.dart
@@ -1,5 +1,8 @@
 abstract class AppFonts {
   const AppFonts._();
 
+  static const String mainFont = _nunito;
+
   static const String roboto = "Roboto";
+  static const String _nunito = "Nunito";
 }
