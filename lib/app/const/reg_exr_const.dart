b4fde9e1519d30f210ba22987462d718bd3f7484
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-12 14:51:08 +0400

    auth: password: rules

--- lib/app/const/reg_exr_const.dart
+++ lib/app/const/reg_exr_const.dart
@@ -2,7 +2,7 @@ abstract class RegExpConst {
   static const String email =
       r"^[a-zA-Zа-яА-Я0-9.a-zA-Zа-яА-Я0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Zа-яА-Я0-9]+\.[a-zA-Zа-яА-Я]+";
   static const String password =
-      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?[{\]}\|^]).{8,}$';
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#*/|\\%+=<>]).{8,}$';
 }
 
 extension StringX on String {
