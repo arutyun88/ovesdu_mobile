fa379df08707ad2d037dac9cdac4d8cafc7f0004
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-14 18:06:59 +0400

    app: loader

--- lib/app/ui/app_loader.dart
+++ lib/app/ui/app_loader.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:ovesdu_mobile/app/data/setting_provider/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class AppLoader extends StatelessWidget {
   const AppLoader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Provider.of<ThemeProvider>(context)
+          .themeData
+          .backgroundColor
+          .withOpacity(.5),
+      child: const Center(
         child: CircularProgressIndicator(),
       ),
     );
