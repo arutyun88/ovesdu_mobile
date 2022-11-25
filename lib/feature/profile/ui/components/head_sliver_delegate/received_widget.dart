c766f4a4c81c63446205a57a8179a613e75c38c7
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-25 22:30:44 +0400

    profile: last visit: ui

--- lib/feature/profile/ui/components/head_sliver_delegate/received_widget.dart
+++ lib/feature/profile/ui/components/head_sliver_delegate/received_widget.dart
@@ -13,7 +13,7 @@ import 'head_sliver_delegate.dart';
 
 const _animationDuration = kThemeAnimationDuration;
 
-class ReceivedWidget extends StatelessWidget {
+class ReceivedWidget extends StatefulWidget {
   const ReceivedWidget({
     Key? key,
     required this.receivedUser,
@@ -34,20 +34,36 @@ class ReceivedWidget extends StatelessWidget {
   final VoidCallback? onTapToBack;
 
   @override
+  State<ReceivedWidget> createState() => _ReceivedWidgetState();
+}
+
+class _ReceivedWidgetState extends State<ReceivedWidget> {
+  late ThemeData theme;
+  late bool avatar;
+  late AppLocalizations dictionary;
+  late String lastVisit;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    theme = Provider.of<ThemeProvider>(context).themeData;
+    avatar = Provider.of<SettingProvider>(context).isCircleAvatar;
+    dictionary = AppLocalizations.of(context)!;
+    lastVisit = DateHelper.wasOnline(context, widget.receivedUser.lastVisit);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeProvider>(context).themeData;
-    final avatar = Provider.of<SettingProvider>(context).isCircleAvatar;
-    final dictionary = AppLocalizations.of(context)!;
     return Stack(
       children: [
         Image.network(
-          receivedUser.image ?? '',
+          widget.receivedUser.image ?? '',
           width: MediaQuery.of(context).size.width,
-          height: expandedHeight,
+          height: widget.expandedHeight,
           fit: BoxFit.cover,
         ),
         Positioned(
-          top: top >= 0 ? top : 0.0,
+          top: widget.top >= 0 ? widget.top : 0.0,
           child: AnimatedContainer(
             duration: _animationDuration,
             curve: Curves.ease,
@@ -55,7 +71,7 @@ class ReceivedWidget extends StatelessWidget {
             height: titleMaxHeight,
             decoration: BoxDecoration(
               color: theme.backgroundColor,
-              borderRadius: zero > paddingTop + 100
+              borderRadius: widget.zero > widget.paddingTop + 100
                   ? const BorderRadius.vertical(
                       top: Radius.circular(mainPadding),
                     )
@@ -66,18 +82,18 @@ class ReceivedWidget extends StatelessWidget {
                 AnimatedPositioned(
                   duration: _animationDuration,
                   curve: Curves.ease,
-                  top: zero < 220 ? paddingTop + 4.0 : 16.0,
-                  left: zero < 220 ? 128.0 : 24.0,
+                  top: widget.zero < 220 ? widget.paddingTop + 4.0 : 16.0,
+                  left: widget.zero < 220 ? 128.0 : 24.0,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        receivedUser.firstName,
+                        widget.receivedUser.firstName,
                         style: theme.textTheme.headline5,
                       ),
                       Text(
-                        receivedUser.lastName,
+                        widget.receivedUser.lastName,
                         style: theme.textTheme.headline6,
                       ),
                     ],
@@ -88,21 +104,21 @@ class ReceivedWidget extends StatelessWidget {
                   left: 24.0,
                   child: AnimatedOpacity(
                     duration: _animationDuration,
-                    opacity: zero < 220 ? 0.0 : 1.0,
+                    opacity: widget.zero < 220 ? 0.0 : 1.0,
                     curve: Curves.ease,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          '${receivedUser.city}, ${receivedUser.country}',
+                          '${widget.receivedUser.city}, ${widget.receivedUser.country}',
                           style: theme.textTheme.bodyText2,
                         ),
                         Text(
-                          '${ageCalculatedTitle(
+                          ageCalculatedTitle(
                             context,
-                            receivedUser.dateOfBirth,
-                          )} / ${receivedUser.isMale ? dictionary.unmarriedM : dictionary.unmarriedW}',
+                            widget.receivedUser.dateOfBirth,
+                          ),
                           style: theme.textTheme.bodyText2,
                         ),
                       ],
@@ -112,21 +128,28 @@ class ReceivedWidget extends StatelessWidget {
                 AnimatedPositioned(
                   duration: _animationDuration,
                   curve: Curves.ease,
-                  top: zero < 220 ? paddingTop + 4.0 : 24.0,
-                  right: zero > 220 ? 16.0 : 24.0,
-                  child: Text(
-                    dictionary.unsubscribe,
-                    style: theme.textTheme.headline6?.copyWith(
-                      color: AppColors.hintTextColor,
+                  top: widget.zero < 220 ? widget.paddingTop + 0.0 : 24.0,
+                  right: widget.zero > 220 ? 16.0 : 24.0,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width / 3.5),
+                    child: Text(
+                      lastVisit,
+                      maxLines: 2,
+                      textAlign: TextAlign.right,
+                      style: theme.textTheme.bodyText2?.copyWith(
+                        color: AppColors.hintTextColor,
+                        height: 1.0,
+                      ),
                     ),
                   ),
                 ),
                 Positioned(
-                  top: paddingTop + 4,
+                  top: widget.paddingTop + 4,
                   left: 52,
                   child: AnimatedOpacity(
                     duration: _animationDuration,
-                    opacity: zero > 220 ? 0.0 : 1.0,
+                    opacity: widget.zero > 220 ? 0.0 : 1.0,
                     curve: Curves.ease,
                     child: GestureDetector(
                       onTap: () {
@@ -149,7 +172,7 @@ class ReceivedWidget extends StatelessWidget {
                         ),
                         clipBehavior: Clip.hardEdge,
                         child: Image.network(
-                          receivedUser.image ?? '',
+                          widget.receivedUser.image ?? '',
                           fit: BoxFit.cover,
                         ),
                       ),
@@ -161,7 +184,7 @@ class ReceivedWidget extends StatelessWidget {
                   right: 16,
                   child: AnimatedOpacity(
                     duration: _animationDuration,
-                    opacity: zero < 220 ? 0.0 : 1.0,
+                    opacity: widget.zero < 220 ? 0.0 : 1.0,
                     curve: Curves.ease,
                     child: const MessageButton(),
                   ),
@@ -171,7 +194,7 @@ class ReceivedWidget extends StatelessWidget {
           ),
         ),
         Positioned(
-          top: paddingTop + 24,
+          top: widget.paddingTop + 24,
           child: Container(
             width: MediaQuery.of(context).size.width,
             padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -179,7 +202,8 @@ class ReceivedWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 GestureDetector(
-                  onTap: onTapToBack ?? () => Navigator.of(context).pop(),
+                  onTap:
+                      widget.onTapToBack ?? () => Navigator.of(context).pop(),
                   child: const Icon(
                     Icons.arrow_back_ios,
                     size: 24,
@@ -187,7 +211,7 @@ class ReceivedWidget extends StatelessWidget {
                   ),
                 ),
                 GestureDetector(
-                  onTap: blockedOnTap,
+                  onTap: widget.blockedOnTap,
                   child: const Center(
                     child: Icon(
                       Icons.more_horiz,
@@ -206,7 +230,7 @@ class ReceivedWidget extends StatelessWidget {
             duration: _animationDuration * .5,
             height: 2,
             width: MediaQuery.of(context).size.width,
-            color: zero >= 120
+            color: widget.zero >= 120
                 ? AppColors.transparent
                 : AppColors.hintTextColor.withOpacity(.05),
           ),
