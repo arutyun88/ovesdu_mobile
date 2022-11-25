5a3afc6ab5b99a551848d1f7c2751e7cd4a17e56
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-25 22:29:42 +0400

    app: date helper: was online

--- lib/app/helpers/date_helper.dart
+++ lib/app/helpers/date_helper.dart
@@ -4,6 +4,54 @@ import 'package:provider/provider.dart';
 
 import '../data/setting_provider/locale_provider.dart';
 
+abstract class DateHelper {
+  const DateHelper._();
+
+  static String wasOnline(BuildContext context, DateTime date) {
+    final dictionary = AppLocalizations.of(context)!;
+    final currentDate = DateTime.now();
+    final difference = currentDate.difference(date);
+    final currentHours = currentDate.hour + (currentDate.minute / 60);
+
+    final isToday = currentHours - difference.inHours > 0.0;
+    if (isToday) {
+      final hours = currentHours - difference.inHours;
+      if (hours == 1.0) {
+        return dictionary.hourAgo;
+      }
+      if (hours < 0.3) {
+        return dictionary.online;
+      }
+      if (hours <= 0.4) {
+        return dictionary.fifteenMinutesAgo;
+      }
+      if (hours <= 1.0) {
+        return dictionary.lessThanHour;
+      } else {
+        return dictionary.fewHours;
+      }
+    } else {
+      final isYesterday = (currentHours + 24) - difference.inHours > 0;
+      if (isYesterday) {
+        return dictionary.yesterday;
+      } else {
+        var days = ((difference.inHours - currentHours) / 24).truncate() + 1;
+        if (days <= 3) {
+          return dictionary.fewDays;
+        }
+        if (days <= 30) {
+          return dictionary.severalWeeksAgo;
+        }
+        if (days <= 60) {
+          return dictionary.overAMonth;
+        } else {
+          return dictionary.fewMonths;
+        }
+      }
+    }
+  }
+}
+
 String ageCalculatedTitle(BuildContext context, DateTime birthday) {
   final locale = Provider.of<LocaleProvider>(context).locale.languageCode;
   final age = ageCalculate(birthday);
