71325059c12bd4186eb2c8902af185018dea8a1f
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-11-30 21:17:52 +0400

    profile: post: reaction: repository

--- lib/feature/user_post/data/user_posts_repository_impl.dart
+++ lib/feature/user_post/data/user_posts_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:injectable/injectable.dart';
 
 import '../../../app/domain/app_api.dart';
+import '../domain/entity/reaction_type.dart';
 import '../domain/entity/user_post/user_posts_entity.dart';
 import '../domain/user_post_repository.dart';
 import 'dto/user_post/user_posts_dto.dart';
@@ -20,6 +21,20 @@ class UserPostRepositoryImpl implements UserPostRepository {
       final data = response.data['data'];
 
       return UserPostsDto.fromJson(data).toEntity();
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<ReactionType> updatePostReaction(int id, ReactionType type) async {
+    await _api.setHeaderLocale();
+    try {
+      final response = await _api.updatePostReaction(id, type.name);
+
+      final data = response.data['data'];
+
+      return mapToReactionType(data['type']);
     } catch (_) {
       rethrow;
     }
--- lib/feature/user_post/domain/user_post_repository.dart
+++ lib/feature/user_post/domain/user_post_repository.dart
@@ -1,5 +1,8 @@
+import 'entity/reaction_type.dart';
 import 'entity/user_post/user_posts_entity.dart';
 
 abstract class UserPostRepository {
   Future<UserPostsEntity> getUserPosts(int id, int limit, int last);
+
+  Future<ReactionType> updatePostReaction(int id, ReactionType type);
 }
