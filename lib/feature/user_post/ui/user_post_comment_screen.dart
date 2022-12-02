72e3c5a0566ba893024008302694c76d7cf4c6a5
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-12-03 02:54:29 +0400

    comment: ui: render box text field

--- lib/feature/user_post/ui/user_post_comment_screen.dart
+++ lib/feature/user_post/ui/user_post_comment_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../app/const/const.dart';
 import '../../../app/data/setting_provider/theme_provider.dart';
+import '../../../app/ui/components/text_fields/app_text_field.dart';
 import '../../../app/ui/config/app_colors.dart';
 import '../domain/entity/user_post/user_post_entity.dart';
 import 'components/user_post_item/user_post_item_content.dart';
 import 'components/user_post_item/user_post_item_header.dart';
 import 'components/user_post_item_statistic/user_post_item_statistic.dart';
 
-class UserPostCommentScreen extends StatelessWidget {
+class UserPostCommentScreen extends StatefulWidget {
   const UserPostCommentScreen({
     Key? key,
     required this.avatar,
@@ -20,53 +22,126 @@ class UserPostCommentScreen extends StatelessWidget {
   final UserPostEntity post;
 
   @override
+  State<UserPostCommentScreen> createState() => _UserPostCommentScreenState();
+}
+
+class _UserPostCommentScreenState extends State<UserPostCommentScreen> {
+  late double fieldHeight = 100.0;
+  late ThemeData theme;
+  late GlobalKey _commentFieldKey;
+  late RenderBox? renderBox;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _commentFieldKey = GlobalKey();
+    renderBox = null;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    theme = Provider.of<ThemeProvider>(context, listen: false).themeData;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeProvider>(context).themeData;
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Container(
-              color: theme.backgroundColor,
-              padding: const EdgeInsets.only(left: 24.0),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.of(context).pop(),
-                    child: const Icon(
-                      Icons.arrow_back_ios,
-                      size: 24,
-                      color: AppColors.orange,
+            Column(
+              children: [
+                Container(
+                  color: theme.backgroundColor,
+                  padding: const EdgeInsets.only(left: 24.0),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.of(context).pop(),
+                        child: const Icon(
+                          Icons.arrow_back_ios,
+                          size: 24,
+                          color: AppColors.orange,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 60,
+                        child: UserPostItemHeader(
+                          avatar: widget.avatar,
+                          firstName: widget.post.author.firstName,
+                          lastName: widget.post.author.lastName,
+                          created: widget.post.created,
+                          updated: widget.post.updated,
+                          lastVisit: widget.lastVisit,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Hero(
+                          transitionOnUserGestures: true,
+                          tag: '${widget.post.id}:'
+                              '${widget.post.author.lastName}.'
+                              '${widget.post.author.firstName}',
+                          child: Column(
+                            children: [
+                              UserPostItemContent(
+                                post: widget.post,
+                                isCommentScreen: true,
+                              ),
+                              UserPostItemStatistic(
+                                avatar: widget.avatar,
+                                post: widget.post,
+                                lastVisit: widget.lastVisit,
+                                isCommentScreen: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: List.generate(
+                            10,
+                            (index) => Container(
+                              height: 100,
+                              color: index % 2 == 0
+                                  ? AppColors.red
+                                  : AppColors.purple,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: fieldHeight),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width - 60,
-                    child: UserPostItemHeader(
-                      avatar: avatar,
-                      firstName: post.author.firstName,
-                      lastName: post.author.lastName,
-                      created: post.created,
-                      updated: post.updated,
-                      lastVisit: lastVisit,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Hero(
-              transitionOnUserGestures: true,
-              tag:
-                  '${post.id}:${post.author.lastName}.${post.author.firstName}',
-              child: Column(
-                children: [
-                  UserPostItemContent(post: post, isCommentScreen: true),
-                  UserPostItemStatistic(
-                    avatar: avatar,
-                    post: post,
-                    lastVisit: lastVisit,
-                    isCommentScreen: true,
-                  ),
-                ],
+            Positioned(
+              bottom: 0.0,
+              child: Container(
+                key: _commentFieldKey,
+                width: MediaQuery.of(context).size.width,
+                color: theme.backgroundColor,
+                padding: const EdgeInsets.all(verticalPadding),
+                // todo multiline
+                child: AppTextField(
+                  keyboardType: TextInputType.multiline,
+                  onChanged: (value) {
+                    if (value.endsWith('\n')) {
+                      renderBox ??= _commentFieldKey.currentContext!
+                          .findRenderObject() as RenderBox;
+                      setState(() {
+                        fieldHeight = 40 + (renderBox?.size.height ?? 0);
+                      });
+                    }
+                  },
+                ),
               ),
             ),
           ],
