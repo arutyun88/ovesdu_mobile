696d2f7614d5d651294e4e8ff217945469669fb3
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-12-03 17:13:20 +0400

    app: dialog: refactoring and adding message dialog

--- lib/app/ui/components/custom_dialog/custom_dialog.dart
+++ lib/app/ui/components/custom_dialog/custom_dialog.dart
@@ -20,6 +20,57 @@ abstract class CustomDialog {
       builder: (context) => const _BlockDialog(),
     );
   }
+
+  static Future<dynamic> showMessageDialog(
+    BuildContext context,
+    String message,
+  ) {
+    final barrierColor = Provider.of<ThemeProvider>(context, listen: false)
+        .themeData
+        .backgroundColor
+        .withOpacity(.7);
+
+    return showDialog(
+      context: context,
+      barrierColor: barrierColor,
+      barrierDismissible: false,
+      builder: (context) => _MessageDialog(message: message),
+    );
+  }
+}
+
+class _MessageDialog extends StatelessWidget {
+  const _MessageDialog({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context).themeData;
+    final dictionary = AppLocalizations.of(context)!;
+
+    return Center(
+      child: _DecoratedDialog(
+        body: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: theme.textTheme.headline6,
+        ),
+        actions: [
+          EmptyButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(
+              dictionary.understand,
+              style: theme.textTheme.headline6,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class _BlockDialog extends StatelessWidget {
@@ -33,74 +84,94 @@ class _BlockDialog extends StatelessWidget {
     final dictionary = AppLocalizations.of(context)!;
 
     return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(mainPadding),
-        child: Container(
-          clipBehavior: Clip.hardEdge,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            color: theme.backgroundColor,
-            borderRadius: BorderRadius.circular(mainPadding),
-            boxShadow: const [
-              BoxShadow(color: AppColors.hintTextColor, blurRadius: 5.0),
-            ],
+      child: _DecoratedDialog(
+        body: Column(
+          children: [
+            Text(
+              dictionary.sureWantToBlock,
+              textAlign: TextAlign.center,
+              style: theme.textTheme.headline6,
+            ),
+            const SizedBox(height: mainPadding),
+            Text(
+              dictionary.wontSeePage,
+              textAlign: TextAlign.center,
+              style: theme.textTheme.bodyText1,
+            ),
+          ],
+        ),
+        actions: [
+          EmptyButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text(
+              dictionary.cancel,
+              style: theme.textTheme.headline6,
+            ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
+          const SizedBox(width: verticalPadding),
+          EmptyButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: Text(
+              dictionary.block,
+              style: theme.textTheme.headline6,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _DecoratedDialog extends StatelessWidget {
+  const _DecoratedDialog({
+    Key? key,
+    required this.body,
+    required this.actions,
+  }) : super(key: key);
+
+  final Widget body;
+  final List<Widget> actions;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context).themeData;
+    return Padding(
+      padding: const EdgeInsets.all(mainPadding),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: theme.backgroundColor,
+          borderRadius: BorderRadius.circular(mainPadding),
+          boxShadow: const [
+            BoxShadow(color: AppColors.hintTextColor, blurRadius: 5.0),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: mainPadding,
+              ),
+              child: body,
+            ),
+            const SizedBox(height: mainPadding / 2),
+            Container(
+              color: theme.hintColor.withOpacity(.1),
+              child: Padding(
                 padding: const EdgeInsets.symmetric(
                   vertical: 16.0,
-                  horizontal: mainPadding,
+                  horizontal: 24.0,
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      dictionary.sureWantToBlock,
-                      textAlign: TextAlign.center,
-                      style: theme.textTheme.headline6,
-                    ),
-                    const SizedBox(height: mainPadding),
-                    Text(
-                      dictionary.wontSeePage,
-                      textAlign: TextAlign.center,
-                      style: theme.textTheme.bodyText1,
-                    ),
-                  ],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: actions,
                 ),
               ),
-              const SizedBox(height: mainPadding / 2),
-              Container(
-                color: theme.hintColor.withOpacity(.1),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 16.0,
-                    horizontal: 24.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      EmptyButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: Text(
-                          dictionary.cancel,
-                          style: theme.textTheme.headline6,
-                        ),
-                      ),
-                      const SizedBox(width: verticalPadding),
-                      EmptyButton(
-                        onPressed: () => Navigator.of(context).pop(true),
-                        child: Text(
-                          dictionary.block,
-                          style: theme.textTheme.headline6,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
