b6a290afb37a81c423da4ba189785fa5b691dd20
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-12-05 15:46:57 +0400

    comment: ui: reaction

--- lib/feature/user_post/ui/components/user_comment/user_comment_item.dart
+++ lib/feature/user_post/ui/components/user_comment/user_comment_item.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../../app/const/const.dart';
 import '../../../../../app/data/setting_provider/theme_provider.dart';
 import '../../../../../app/ui/config/app_colors.dart';
+import '../../../../profile/ui/components/item_divider.dart';
 import '../../../domain/entity/user_post_comment/user_post_comment_entity.dart';
 import 'user_comment_header.dart';
 import 'user_comment_item_statistic.dart';
 
 class UserCommentItem extends StatelessWidget {
-  const UserCommentItem(this.comment, {Key? key}) : super(key: key);
+  const UserCommentItem(
+    this.comment, {
+    Key? key,
+    this.replyToComment,
+  }) : super(key: key);
 
   final UserPostCommentEntity comment;
+  final UserPostCommentEntity? replyToComment;
 
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeProvider>(context).themeData;
+    final dictionary = AppLocalizations.of(context)!;
+
     return Column(
       children: [
         Padding(
@@ -30,6 +39,53 @@ class UserCommentItem extends StatelessWidget {
                 updated: comment.updated,
                 lastVisit: comment.author.lastVisit,
               ),
+              if (replyToComment != null)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: verticalPadding,
+                    horizontal: verticalPadding * 2,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: verticalPadding,
+                      horizontal: verticalPadding * 2,
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: AppColors.hintTextColor.withOpacity(.07),
+                      borderRadius: BorderRadius.circular(mainRadius / 3),
+                      border: Border.all(
+                        width: 1,
+                        color: AppColors.hintTextColor.withOpacity(.3),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          dictionary
+                              .responseToComment(comment.author.firstName),
+                          style: theme.textTheme.bodyText2?.copyWith(
+                            color: AppColors.hintTextColor,
+                          ),
+                        ),
+                        const Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: verticalPadding),
+                          child: ItemDivider(
+                            padding: EdgeInsets.zero,
+                          ),
+                        ),
+                        Text(
+                          replyToComment?.text ?? '',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: theme.textTheme.bodyText2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               Container(
                 width: MediaQuery.of(context).size.width,
                 padding: const EdgeInsets.symmetric(
--- lib/feature/user_post/ui/components/user_comment/user_comment_list.dart
+++ lib/feature/user_post/ui/components/user_comment/user_comment_list.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../app/ui/config/app_colors.dart';
+import '../../../domain/entity/user_post_comment/user_post_comment_entity.dart';
 import '../../../domain/state/user_post_comment/user_post_comment_cubit.dart';
 import 'user_comment_item.dart';
 
@@ -20,16 +20,35 @@ class UserCommentList extends StatelessWidget {
             return Column(
               children: List.generate(
                 comments.comments.length,
-                    (index) => UserCommentItem(
-                  comments.comments[index],
-                ),
+                (index) {
+                  final replyComment = comments.comments[index].toCommentId;
+                  UserPostCommentEntity? reply;
+                  if (replyComment != null) {
+                    final tempList = comments.comments
+                        .where(
+                          (element) => element.id == replyComment,
+                        )
+                        .toList();
+                    if (tempList.isEmpty) {
+                      reply = comments.commentsResponses
+                          .firstWhere((element) => element.id == replyComment);
+                    } else {
+                      reply = tempList.first;
+                    }
+                  }
+
+                  return UserCommentItem(
+                    comments.comments[index],
+                    replyToComment: reply,
+                  );
+                },
               ),
             );
           },
           orElse: () => Column(
             children: List.generate(
               3,
-                  (index) => Container(
+              (index) => Container(
                 height: 100,
                 width: 100,
                 color: AppColors.hintTextColor,
