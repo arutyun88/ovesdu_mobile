a6576f7fc828f18c70ad373151f8634c7d61eb77
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-12-05 13:02:37 +0400

    comment: reaction: ui: change state

--- lib/feature/user_post/ui/components/user_comment/user_comment_item_reaction.dart
+++ lib/feature/user_post/ui/components/user_comment/user_comment_item_reaction.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../../app/data/setting_provider/theme_provider.dart';
+import '../../../domain/entity/reaction_type.dart';
 import '../../../domain/entity/user_post_comment/user_post_comment_entity.dart';
+import '../../../domain/state/user_post_comment/reaction/user_comment_reaction_cubit.dart';
 import '../reaction_widget.dart';
 
 class UserCommentItemReaction extends StatefulWidget {
@@ -41,12 +44,56 @@ class _UserCommentItemReactionState extends State<UserCommentItemReaction> {
 
   @override
   Widget build(BuildContext context) {
-    return ReactionWidget(
-      liked: liked,
-      like: like,
-      dislike: dislike,
-      likeOnPressed: () {},
-      dislikeOnPressed: () {},
+    return BlocListener<UserCommentReactionCubit, UserCommentReactionState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          liked: () => setState(() {
+            if (liked == false) {
+              dislike = dislike - 1;
+            }
+            like = like + 1;
+            liked = true;
+          }),
+          disliked: () => setState(() {
+            if (liked == true) {
+              like = like - 1;
+            }
+            dislike = dislike + 1;
+            liked = false;
+          }),
+          none: () => setState(() {
+            if (liked == true) {
+              like = like - 1;
+            } else {
+              dislike = dislike - 1;
+            }
+            liked = null;
+          }),
+        );
+      },
+      child: ReactionWidget(
+        liked: liked,
+        like: like,
+        dislike: dislike,
+        likeOnPressed: likeOnPressed,
+        dislikeOnPressed: dislikeOnPressed,
+      ),
     );
   }
+
+  void likeOnPressed() =>
+      context.read<UserCommentReactionCubit>().updateCommentReaction(
+            id: widget.comment.id,
+            type: liked == null || liked == false
+                ? ReactionType.like
+                : ReactionType.none,
+          );
+
+  void dislikeOnPressed() =>
+      context.read<UserCommentReactionCubit>().updateCommentReaction(
+            id: widget.comment.id,
+            type: liked == null || liked == true
+                ? ReactionType.dislike
+                : ReactionType.none,
+          );
 }
--- lib/feature/user_post/ui/components/user_comment/user_comment_item_statistic.dart
+++ lib/feature/user_post/ui/components/user_comment/user_comment_item_statistic.dart
@@ -4,9 +4,12 @@ import 'package:provider/provider.dart';
 
 import '../../../../../app/const/const.dart';
 import '../../../../../app/data/setting_provider/theme_provider.dart';
+import '../../../../../app/di/init_di.dart';
 import '../../../../../app/helpers/helpers.dart';
 import '../../../../../app/ui/config/app_colors.dart';
 import '../../../domain/entity/user_post_comment/user_post_comment_entity.dart';
+import '../../../domain/state/user_post_comment/reaction/user_comment_reaction_cubit.dart';
+import '../../../domain/user_post_repository.dart';
 import 'user_comment_item_reaction.dart';
 
 class UserCommentItemStatistic extends StatelessWidget {
@@ -45,7 +48,13 @@ class UserCommentItemStatistic extends StatelessWidget {
                 ),
               ),
             const Spacer(),
-            UserCommentItemReaction(comment),
+            Provider(
+              create: (_) => UserCommentReactionCubit(
+                locator.get<UserPostRepository>(),
+              ),
+              builder: (context, child) => UserCommentItemReaction(comment),
+              child: UserCommentItemReaction(comment),
+            ),
           ],
         ),
       ),
