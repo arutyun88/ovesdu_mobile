113ad869e8ba4fe86025735c38b9f1c368b48324
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2022-12-06 13:53:00 +0400

    comment: ui: replied to comment item

--- lib/feature/user_post/ui/components/user_comment/user_comment_item.dart
+++ lib/feature/user_post/ui/components/user_comment/user_comment_item.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../../app/const/const.dart';
 import '../../../../../app/data/setting_provider/theme_provider.dart';
 import '../../../../../app/ui/config/app_colors.dart';
-import '../../../../../app/ui/components/item_divider.dart';
 import '../../../domain/entity/user_post_comment/user_post_comment_entity.dart';
 import 'user_comment_header.dart';
 import 'user_comment_item_statistic.dart';
+import 'user_comment_replied_item.dart';
 
 class UserCommentItem extends StatelessWidget {
   const UserCommentItem(
@@ -25,7 +24,6 @@ class UserCommentItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeProvider>(context).themeData;
-    final dictionary = AppLocalizations.of(context)!;
 
     return Column(
       children: [
@@ -35,61 +33,9 @@ class UserCommentItem extends StatelessWidget {
             children: [
               UserCommentHeader(comment: comment),
               if (replyToCommentText != null)
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: verticalPadding,
-                    horizontal: verticalPadding * 2,
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: verticalPadding,
-                      horizontal: verticalPadding * 2,
-                    ),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: AppColors.hintTextColor.withOpacity(.07),
-                      borderRadius: BorderRadius.circular(mainRadius / 3),
-                      border: Border.all(
-                        width: 1,
-                        color: AppColors.hintTextColor.withOpacity(.3),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (replyToCommentAuthor != null)
-                          Text(
-                            dictionary
-                                .responseToComment(replyToCommentAuthor ?? ''),
-                            style: theme.textTheme.bodyText2?.copyWith(
-                              color: AppColors.hintTextColor,
-                            ),
-                          ),
-                        if (replyToCommentAuthor != null)
-                          const Padding(
-                            padding:
-                                EdgeInsets.symmetric(vertical: verticalPadding),
-                            child: ItemDivider(
-                              padding: EdgeInsets.zero,
-                              color: AppColors.hintTextColor,
-                            ),
-                          ),
-                        Text(
-                          replyToCommentText ?? '',
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: theme.textTheme.bodyText2?.copyWith(
-                            color: replyToCommentAuthor == null
-                                ? AppColors.hintTextColor.withOpacity(.9)
-                                : theme.textTheme.bodyText2?.color,
-                            fontStyle: replyToCommentAuthor == null
-                                ? FontStyle.italic
-                                : FontStyle.normal,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                UserCommentRepliedItem(
+                  replyToCommentAuthor: replyToCommentAuthor,
+                  replyToCommentText: replyToCommentText,
                 ),
               Container(
                 width: MediaQuery.of(context).size.width,
