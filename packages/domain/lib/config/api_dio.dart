02c47d0f2a3da9a1569886cf816de9339570a4a0
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-01-25 11:24:27 +0400

    domain: api_dio interface

---
+++ packages/domain/lib/config/api_dio.dart
@@ -0,0 +1,5 @@
+import 'package:dio/dio.dart';
+
+abstract class ApiDio {
+  Dio get dio;
+}
--- packages/domain/lib/domain.dart
+++ packages/domain/lib/domain.dart
@@ -1,4 +1,5 @@
 export 'state/auth_state/auth_cubit.dart';
 export 'config/app_config.dart' show AppConfig;
+export 'config/api_dio.dart' show ApiDio;
 
 export './di/di.dart' show configureDependencies;
