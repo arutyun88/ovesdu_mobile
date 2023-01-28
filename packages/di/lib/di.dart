5013c20e7b64aa1cc8444ecd4369684d271ffc47
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-01-28 16:13:36 +0400

    di: config for different environment

--- packages/di/lib/di.dart
+++ packages/di/lib/di.dart
@@ -1,10 +1,20 @@
+import 'package:data/config/prod_app_config.dart';
+import 'package:data/config/test_app_config.dart';
 import 'package:data/data.dart';
 import 'package:domain/domain.dart';
 import 'package:get_it/get_it.dart';
 
-void configureDependencies(GetIt instance) {
+void configureDependencies(GetIt instance, String environment) {
   /// CONFIGURATIONS
-  instance.registerSingleton<AppConfig>(DevAppConfig());
+  if (environment == 'dev') {
+    instance.registerSingleton<AppConfig>(DevAppConfig());
+  } else if (environment == 'prod') {
+    instance.registerSingleton<AppConfig>(ProdAppConfig());
+  } else {
+    instance.registerSingleton<AppConfig>(TestAppConfig());
+  }
+
+  /// API
   instance.registerSingleton<ApiDio>(ApiDioImpl(instance()));
   instance.registerSingleton<AppApi>(DioAppApi(instance()));
   instance.registerSingleton<AuthApi>(DioAuthApi(instance()));
