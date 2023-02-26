0753d34ab23f8f3ba255e23e67e9edcdb1451ecc
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-02-26 12:21:29 +0400

    data: replaced the http package with the dio package

--- packages/data/lib/datasources/authentication_remote_datasource.dart
+++ packages/data/lib/datasources/authentication_remote_datasource.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:data/exceptions/exceptions.dart';
 import 'package:data/models/name_model.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 abstract class AuthenticationRemoteDatasource {
   /// Calls the /auth/info/{[usernameOrEmailOrPhoneNumber]} endpoint
@@ -13,22 +13,23 @@ abstract class AuthenticationRemoteDatasource {
 
 class AuthenticationRemoteDatasourceImpl
     implements AuthenticationRemoteDatasource {
-  final http.Client _client;
+  final Dio _client;
 
   const AuthenticationRemoteDatasourceImpl({
-    required http.Client client,
+    required Dio client,
   }) : _client = client;
 
   @override
   Future<NameModel> getNameIfItExist(
     String usernameOrEmailOrPhoneNumber,
   ) async {
-    final response = await _client.get(
-      Uri(path: '/auth/info/$usernameOrEmailOrPhoneNumber'),
-      headers: {'Content-Type': 'application/json'},
-    );
-    if(response.statusCode == 200) {
-      return NameModel.fromJson(jsonDecode(response.body)['data']);
+    final response =
+        await _client.get('/auth/info/$usernameOrEmailOrPhoneNumber')
+          ..requestOptions.headers = {
+            Headers.contentTypeHeader: Headers.jsonContentType,
+          };
+    if (response.statusCode == 200) {
+      return NameModel.fromJson(jsonDecode(response.data)['data']);
     } else {
       throw ServerException();
     }
--- packages/data/test/datasources/authentication_remote_datasource_test.dart
+++ packages/data/test/datasources/authentication_remote_datasource_test.dart
@@ -6,48 +6,50 @@ import 'package:data/models/name_model.dart';
 import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 import '../resources/resources_reader.dart';
 import 'authentication_remote_datasource_test.mocks.dart';
 
-@GenerateMocks([http.Client])
+@GenerateMocks([Dio])
 void main() {
   late AuthenticationRemoteDatasource datasource;
-  late MockClient client;
+  late Dio client;
 
   setUp(() {
-    client = MockClient();
+    client = MockDio();
     datasource = AuthenticationRemoteDatasourceImpl(client: client);
   });
 
-  void setUpMockClientSuccess200() =>
-      when(client.get(any, headers: anyNamed('headers'))).thenAnswer(
-          (_) async => http.Response(reader('name_data_success'), 200));
+  final tNameModel =
+      NameModel.fromJson(jsonDecode(reader('name_data_success'))['data']);
+  final qName = tNameModel.name;
+  final path = '/auth/info/$qName';
+
+  Response response(String value, int code) => Response(
+        requestOptions: RequestOptions(path: path),
+        statusCode: code,
+        data: value,
+      );
+
+  void setUpMockClientSuccess200() => when(client.get(path)).thenAnswer(
+      (_) async => Future.value(response(reader('name_data_success'), 200)));
 
   group('getNameIfItExist', () {
-    final qName = 'some';
-    final tNameModel = NameModel.fromJson(
-      jsonDecode(reader('name_data_success'))['data'],
-    );
-
     test(
       '''should perform GET request on a URL with username or email 
-      or phone number being the endpoint and with application/json header''',
-      () {
+      or phone number being the endpoint, once''',
+      () async {
         setUpMockClientSuccess200();
 
         datasource.getNameIfItExist(qName);
 
-        verify(client.get(
-          Uri(path: '/auth/info/$qName'),
-          headers: {'Content-Type': 'application/json'},
-        ));
+        verify(client.get(path)).called(1);
       },
     );
 
     test(
-      'should return NameEntity when the response code is 200',
+      'should return NameModel when the response code is 200',
       () async {
         setUpMockClientSuccess200();
 
@@ -60,8 +62,8 @@ void main() {
     test(
       'should throw a ServerException when the response code is 404 or other',
       () async {
-        when(client.get(any, headers: anyNamed('headers'))).thenAnswer(
-            (_) async => http.Response(reader('name_data_error'), 404));
+        when(client.get(path)).thenAnswer((_) async =>
+            Future.value(response(reader('name_data_error'), 404)));
 
         final call = datasource.getNameIfItExist;
 
